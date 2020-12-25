a851fe30b3e2c8362a8c0f89f2ea6004efed5fa2
Neovav <neovav@outlook.com>
2020-12-25 18:11:15 +0200

    Add methods for saved search resumes

--- src/Resumes.php
+++ src/Resumes.php
@@ -8,6 +8,8 @@ class Resumes
 {
     const QUERY_SEARCH = '/resumes';
 
+    const QUERY_SAVED_SEARCH = '/saved_searches/resumes';
+
     private Api $api;
 
     /**
@@ -38,6 +40,139 @@ class Resumes
         if (!empty($queryString)) {
             $url .= "?$queryString";
         }
+
+        $headers = ['Content-type' => 'application/x-www-form-urlencoded'];
+        $response = $this->api->request($url, $headers);
+
+        $body = $response->getBody()->getContents();
+
+        $data = json_decode($body, true);
+
+        if (!is_array($data) || empty($data)) {
+            throw new \Exception('Error response searching vacancies');
+        }
+
+        return $data;
+    }
+
+    /**
+     * Searching resumes
+     *
+     * @param ResumesQuery $query
+     *
+     * @return array
+     *
+     * @throws
+     */
+    public function createSavedSearch(ResumesQuery $query):array
+    {
+        $url = 'https://' . Api::HOST_API . self::QUERY_SAVED_SEARCH;
+
+        $body = Utils::convertClassToJson($query);
+
+        $headers = ['Content-type' => 'application/json'];
+        $response = $this->api->requestPost($url, $body, $headers);
+
+        $location = $response->getHeader('Location');
+
+        if (empty($location)) {
+            throw new \Exception('Error request.');
+        }
+
+        return $location;
+    }
+
+    /**
+     * Delete saved search resumes
+     *
+     * @param int $id
+     *
+     * @return Resumes
+     *
+     * @throws
+     */
+    public function deleteSavedSearch(int $id): Resumes
+    {
+        $url = 'https://' . Api::HOST_API . self::QUERY_SAVED_SEARCH . "/$id";
+
+        $headers = [];
+        $response = $this->api->request($url, $headers, 'DELETE');
+
+        $requestCode = $response->getStatusCode();
+
+        if ($requestCode !== 204) {
+            throw new \Exception('Error request.');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Move saved search resumes to another manager
+     *
+     * @param int $id
+     * @param int $managerId
+     *
+     * @return Resumes
+     *
+     * @throws
+     */
+    public function moveSavedSearchToManagerId(int $id, int $managerId): Resumes
+    {
+        $url = 'https://' . Api::HOST_API . self::QUERY_SAVED_SEARCH . "/$id/managers/$managerId";
+
+        $headers = [];
+        $response = $this->api->request($url, $headers, 'PUT');
+
+        $requestCode = $response->getStatusCode();
+
+        if ($requestCode !== 204) {
+            throw new \Exception('Error request.');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get saved searches resume
+     *
+     * @param int $page
+     * @param int $perPage
+     *
+     * @return array
+     *
+     * @throws
+     */
+    public function savedSearch(int $page = 1, int $perPage = 10):array
+    {
+        $url = 'https://' . Api::HOST_API . self::QUERY_SAVED_SEARCH . "?page=$page&per_page=$perPage";
+
+        $headers = ['Content-type' => 'application/x-www-form-urlencoded'];
+        $response = $this->api->request($url, $headers);
+
+        $body = $response->getBody()->getContents();
+
+        $data = json_decode($body, true);
+
+        if (!is_array($data) || empty($data)) {
+            throw new \Exception('Error response searching vacancies');
+        }
+
+        return $data;
+    }
+
+    /**
+     * Get saved searches resume by id
+     *
+     * @param int $id
+     *
+     * @return array
+     *
+     * @throws
+     */
+    public function savedSearchById(int $id):array
+    {
+        $url = 'https://' . Api::HOST_API . self::QUERY_SAVED_SEARCH . "/$id";
 
         $headers = ['Content-type' => 'application/x-www-form-urlencoded'];
         $response = $this->api->request($url, $headers);
